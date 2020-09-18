8246fbc2c7729fe725d1b2488b497da16adbc41c
Nicolas Martinez <nmartinez@applaudostudios.com>
2020-09-17 18:51:41 -0600

    ✨ Adds page to start auth flow

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { getAuthorize } from "../../api/apiClient";
 
 const Home = () => {
-  return <div className="home-container">Home container</div>;
+  const handleAuthorization = () => {
+    getAuthorize()
+      .then((response) => {
+        const { data } = response;
+        window.location.href = data.url;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="home-container">
+      Home container
+      <button onClick={handleAuthorization}>start flow</button>
+    </div>
+  );
 };
 
 export default Home;
