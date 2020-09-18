40c5402a2a8fdbb173b40515f35440d4b398983d
Nicolas Martinez <nmartinez@applaudostudios.com>
2020-09-17 18:52:11 -0600

    ✨ Adds page to recieve auth code

--- src/pages/callback/Callback.js
+++ src/pages/callback/Callback.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { getAccessToken } from "../../api/apiClient";
 
 const Callback = () => {
-  return <div className="callback-container">este es el callback</div>;
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  let query = new URLSearchParams(useLocation().search);
+
+  const getAccesTokenFromServer = () => {
+    const code = query.get("code");
+    const state = query.get("state");
+    getAccessToken(code, state)
+      .then((response) => {
+        setLoading(false);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError(true);
+      });
+  };
+
+  useEffect(() => {
+    getAccesTokenFromServer();
+  });
+
+  if (loading) {
+    return <span>Cargando...</span>;
+  } else if (!loading && error) {
+    return <span>Ocurrio un error con su authenticacion: </span>;
+  }
 };
 
 export default Callback;
